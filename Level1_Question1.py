96419bcd7206f19a50ebeb436a8e5a6afe4185ca
me10b031 <karrasakethreddy@gmail.com>
2018-06-03 00:34:29 -0500

    Initial commit

---
+++ Level1_Question1.py
@@ -0,0 +1,13 @@
+
+"""program which will find all such numbers which are divisible by 7 but are not a multiple of 5,
+between 2000 and 3200 (both included).
+The numbers obtained should be printed in a comma-separated sequence on a single line"""
+
+
+list = []
+for item in range (2000,3201):
+    if item % 7 == 0 and item % 5 != 0:
+        list.append(item)
+print (str(list)[1:-1])
+
+
