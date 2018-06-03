472279b3ec282e4f7503404e97587cd459234ed3
me10b031 <karrasakethreddy@gmail.com>
2018-06-03 11:37:38 -0500

    Q3 Final

--- Level2_Question3.py
+++ Level2_Question3.py
@@ -2,3 +2,9 @@
 such that is an integral number between 1 and n (both included). and then the program should print the dictionary'''
 
 
+a = int(input("please enter the number"))
+dict = {}
+for i in range (1,a+1):
+    dict[i]= i*i
+
+print (dict)
