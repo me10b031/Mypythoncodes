c4edb5b32e54bda067c115be7a56bf85d0fcda18
me10b031 <karrasakethreddy@gmail.com>
2018-06-05 18:37:11 -0500

    Q13 Final

--- Level2_Question13.py
+++ Level2_Question13.py
@@ -1,9 +1,4 @@
-'''Write a program that accepts a sentence and calculate the number of letters and digits.
-Suppose the following input is supplied to the program:
-hello world! 123
-Then, the output should be:
-LETTERS 10
-DIGITS 3'''
+'''Write a program that accepts a sentence and calculate the number of letters and digits.'''
 
 Letters = 0
 Digits = 0
