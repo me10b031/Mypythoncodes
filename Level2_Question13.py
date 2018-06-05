128fa6420f81883305231a3421cd68a24b97e383
me10b031 <karrasakethreddy@gmail.com>
2018-06-05 18:36:41 -0500

    Q13 Final

---
+++ Level2_Question13.py
@@ -0,0 +1,23 @@
+'''Write a program that accepts a sentence and calculate the number of letters and digits.
+Suppose the following input is supplied to the program:
+hello world! 123
+Then, the output should be:
+LETTERS 10
+DIGITS 3'''
+
+Letters = 0
+Digits = 0
+import string
+input = input("Please Enter Something")
+
+a = str(input)
+
+for m in a:
+    if m in list(string.ascii_lowercase):
+        Letters = Letters + 1
+
+    if str(m) in ['0','1','2','3','4','5','6','7','8','9']:
+        Digits = Digits + 1
+
+print('LETTERS {0}'.format(Letters))
+print('DIGITS {0}'.format(Digits))
